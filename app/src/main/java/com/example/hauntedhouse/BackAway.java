95e404534d167f7cd3dac0827b698602cf71a40c
NaiaCrump-CodeNext <naiacrump@gmail.com>
2019-08-07 14:54:05 -0400

    zLayout Update 2

--- app/src/main/java/com/example/hauntedhouse/BackAway.java
+++ app/src/main/java/com/example/hauntedhouse/BackAway.java
@@ -11,4 +11,6 @@ public class BackAway extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back_away);
     }
+
+    public void
 }
--- app/src/main/java/com/example/hauntedhouse/BsBeBonkers.java
+++ app/src/main/java/com/example/hauntedhouse/BsBeBonkers.java
@@ -4,16 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AlmostDone extends AppCompatActivity {
+public class BsBeBonkers extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_leave_box);
-    }
-
-    public void runTowardsDoors(View view){
-        Intent intent = new Intent(View view);
-        startActivity(intent);
+        setContentView(R.layout.activity_bs_be_bonkers);
     }
 }
