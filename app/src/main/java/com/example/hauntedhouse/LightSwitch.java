b62007fb66f9b32ac464fd42cfb08c16c7e0e673
BGC-301 <naiacrump@gmail.com>
2019-08-09 13:13:41 -0400

    Layout Update 4: NO ENDINGS

--- app/src/main/java/com/example/hauntedhouse/LightSwitch.java
+++ app/src/main/java/com/example/hauntedhouse/LightSwitch.java
@@ -9,6 +9,6 @@ public class LightSwitch extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_turn_light_on);
+        setContentView(R.layout.activity_turn_light_on2);
     }
 }
